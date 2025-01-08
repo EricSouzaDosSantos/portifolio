354faa820d7412360e230388aedc30e479ed33d2
Eric <ericsouza94895@gmail.com>
2025-01-07 23:44:20 -0300

    style: deleting line

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-root',
