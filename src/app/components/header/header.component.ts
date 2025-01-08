38323ffd55c1653fc69bf7e2a1105fc5bc373122
Eric <ericsouza94895@gmail.com>
2025-01-07 23:43:23 -0300

    feat: add NgStyle and function to active menu hamburguer

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,19 @@
+import { NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  menuOpen: boolean = false;
+  borderRadius: string = "0 0 10px 10px"
 
+  toggleMenu(): void {
+    this.menuOpen = !this.menuOpen;
+    this.borderRadius = this.menuOpen ? "0" : "0 0 10px 10px";
+  }
 }
+
