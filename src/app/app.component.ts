affca99c9ce825ba7a33d3bfbbf85ec79ffbcb29
Eric <ericsouza94895@gmail.com>
2025-01-08 23:48:58 -0300

    feat: calling the main component in app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
-import { NgStyle } from '@angular/common';
+import { MainComponent } from './components/main/main.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [HeaderComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
