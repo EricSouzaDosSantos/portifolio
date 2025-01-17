d06aacd5c4fbb97c99db7baef7963360c9bb91da
Eric <ericsouza94895@gmail.com>
2025-01-16 22:22:17 -0300

    feat: add functionality to check screen size and adding variable with my information

--- src/app/components/main/about-me/about-me.component.ts
+++ src/app/components/main/about-me/about-me.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, ElementRef } from '@angular/core';
+import { Component, HostListener, ElementRef, OnInit } from '@angular/core';
 import { NgStyle, NgClass, CommonModule } from '@angular/common';
 
 
@@ -8,23 +8,60 @@ import { NgStyle, NgClass, CommonModule } from '@angular/common';
   templateUrl: './about-me.component.html',
   styleUrls: ['./about-me.component.scss']
 })
-export class AboutMeComponent {
+export class AboutMeComponent implements OnInit {
   isBadgeVisible = false;
   lastScrollTop = 0;
   isModalVisible = false;
   selectedSkill = '';
   technologies: string[] = [];
+  informations: string[] = [
+    'ericsouzadossantos28@gmail.com',
+    '(11) 96070-3544',
+    'São Paulo - SP'
+  ]
   skills = [
-    { name: 'Java', icon: 'fa-java', progress: 90 },
-    { name: 'Angular', icon: 'fa-angular', progress: 30 },
-    { name: 'Spring', icon: 'fa-java', progress: 80 },
-    { name: 'SCSS', icon: 'fa-sass', progress: 40 },
-    { name: 'Git', icon: 'fa-git', progress: 85 },
-    { name: 'AWS', icon: 'fa-aws', progress: 50 },
+    { name: 'Java', icon: 'fab fa-java', progress: 90, color: '#f89820' },
+    { name: 'Angular', icon: 'fab fa-angular', progress: 30, color: '#dd0031' },
+    { name: 'Spring', icon: 'fa-solid fa-leaf', progress: 80, color: '#4caf50' },
+    { name: 'SCSS', icon: 'fab fa-sass', progress: 40, color: '#c76395' },
+    { name: 'Git', icon: 'fab fa-git', progress: 85, color: '#f1502f' },
+    { name: 'AWS', icon: 'fab fa-aws', progress: 50, color: '#ff9900' },
   ];
+  isHovered: boolean = false;
+  hoveredIndex: number | null = null;
+  isMobile: boolean = false;
+  showHighlights: boolean = false;
+
+  ngOnInit(): void {
+    this.checkViewport();
+  }
+
+  @HostListener('window:resize', [])
+  onResize() {
+    this.checkViewport();
+  }
+
+  private checkViewport() {
+    this.isMobile = window.innerWidth <= 768; 
+  }
+
+  toggleHighlights() {
+    if (this.isMobile) {
+      this.showHighlights = !this.showHighlights;
+    }
+  }
   
+  // getProgressStyle(progress: number): any {
+  //   return {
+  //     width: `${progress}%`,
+  //     animation: 'fill-bar 1.5s ease forwards',
+  //   };
+  // }
 
   constructor(private el: ElementRef) {}
+  // ngOnInit(): void {
+  //   throw new Error('Method not implemented.');
+  // }
 
   @HostListener('window:scroll', [])
   onScroll(): void {
@@ -85,4 +122,6 @@ export class AboutMeComponent {
   closeModal(): void {
     this.isModalVisible = false;
   }
+
+  
 }
