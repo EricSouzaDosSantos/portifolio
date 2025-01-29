83660ab15a98bcdc69f19ccd372e61346056ceae
Eric <ericsouza94895@gmail.com>
2025-01-28 23:43:07 -0300

    feat: add projects component in main component

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from "./home/home.component";
 import { AboutMeComponent } from './about-me/about-me.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @Component({
   selector: 'app-main',
-  imports: [HomeComponent, AboutMeComponent],
+  imports: [HomeComponent, AboutMeComponent, ProjectsComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
