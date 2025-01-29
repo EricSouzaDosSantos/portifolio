54965493328690397f07d71ce8caa0d22c3e5909
Eric <ericsouza94895@gmail.com>
2025-01-29 00:00:49 -0300

    feat: add functions open and close modal in ts

--- src/app/components/main/projects/projects.component.ts
+++ src/app/components/main/projects/projects.component.ts
@@ -1,11 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
 
+  projects = [
+    {
+      title: 'Projeto Teste',
+      image: '/my-animation.png',
+      video: '',
+      description: 'Descrição do projeto teste.',
+    },
+    {
+      title: 'Outro Projeto',
+      image: '/my-animation.png',
+      video: '',
+      description: 'Descrição de outro projeto.',
+    },
+  ];
+
+  selectedProject: any = null;
+
+  openModal(project: any) {
+    this.selectedProject = project;
+  }
+
+  closeModal(event: Event) {
+    if ((event.target as HTMLElement).classList.contains('modal')) {
+      this.selectedProject = null;
+    }
+  }
 }
