a281c727a15e74cab33dc6d2df6d50a5edf68b52
Eric <ericsouza94895@gmail.com>
2025-02-02 23:29:59 -0300

    feat: importing contact component

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HomeComponent } from "./home/home.component";
 import { AboutMeComponent } from './about-me/about-me.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
 
 @Component({
   selector: 'app-main',
-  imports: [HomeComponent, AboutMeComponent, ProjectsComponent],
+  imports: [HomeComponent, AboutMeComponent, ProjectsComponent, ContactMeComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
