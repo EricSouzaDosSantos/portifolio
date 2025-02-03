8e5c94ada697a48185e59f7628400a392c6f81fb
Eric <ericsouza94895@gmail.com>
2025-02-02 23:29:12 -0300

    feat: add component to contact

---
+++ src/app/components/main/contact-me/contact-me.component.ts
@@ -0,0 +1,67 @@
+import { Component } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import emailjs from '@emailjs/browser';
+import { environment } from './../../../../environment/environment.prod'; 
+
+
+@Component({
+  selector: 'app-contact-me',
+  standalone: true,
+  imports: [CommonModule, FormsModule, HttpClientModule],
+  templateUrl: './contact-me.component.html',
+  styleUrl: './contact-me.component.scss'
+})
+export class ContactMeComponent {
+  formData = {
+    name: '',
+    email: '',
+    message: '',
+  };
+
+  isSending = false;
+  isSuccess = false;
+  isError = false;
+
+  constructor(private http: HttpClient) {}
+
+  onSubmit() {
+    this.isSending = true;
+    this.isSuccess = false;
+    this.isError = false;
+
+    const messageFormat = {
+      name: this.formData.name,
+      message: `Olá Eric,\n
+    eu me chamo ${this.formData.name},\n
+     ${this.formData.message} \n
+    meu email para contato: ${this.formData.email}`};
+    
+  
+    emailjs.send(
+      environment.emailjs.service_id,
+      environment.emailjs.template_id,
+      messageFormat,
+      environment.emailjs.user_id
+    )      .then(
+        (response) => {
+          this.isSending = false;
+          this.isSuccess = true;
+          this.resetForm();
+        },
+        (error) => {
+          this.isSending = false;
+          this.isError = true;
+        }
+      );
+  }
+
+  resetForm() {
+    this.formData = {
+      name: '',
+      email: '',
+      message: '',
+    };
+  }
+}
