99eab532c9952a391af698e666212c675caa6bd6
Eric <ericsouza94895@gmail.com>
2025-02-08 00:45:35 -0300

    fix: removing space in environments variables

--- mynode.js
+++ mynode.js
@@ -5,9 +5,9 @@ const checkSign = '\u{2705}' ;
 const dotenv = require ( 'dotenv' ). config ({ path : 'src/.env' }); ; 
 
 const envFile = `export const environment = { 
-    EMAILJS_SERVICE_ID: ' ${process.env. EMAILJS_SERVICE_ID} ', 
-    EMAILJS_TEMPLATE_ID: ' ${process.env. EMAILJS_TEMPLATE_ID} ', 
-    EMAILJS_USER_ID: ' ${process.env. EMAILJS_USER_ID} ', 
+    EMAILJS_SERVICE_ID: '${process.env. EMAILJS_SERVICE_ID}', 
+    EMAILJS_TEMPLATE_ID: '${process.env. EMAILJS_TEMPLATE_ID}', 
+    EMAILJS_USER_ID: '${process.env. EMAILJS_USER_ID}', 
 }; 
 ` ; 
 const targetPath = path. join (__dirname, './src/environments/environment.prod.ts' ); 
