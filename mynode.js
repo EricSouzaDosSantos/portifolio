b18b0d63dab54ba09ffc39fed9eec2f0026ed14d
Eric <ericsouza94895@gmail.com>
2025-02-07 23:59:35 -0300

    feat: add dotenv and mynode.js

---
+++ mynode.js
@@ -0,0 +1,21 @@
+const fs = require ( 'fs' ); 
+const path = require ( 'path' ); 
+const successColor = '\x1b[32m%s\x1b[0m' ; 
+const checkSign = '\u{2705}' ; 
+const dotenv = require ( 'dotenv' ). config ({ path : 'src/.env' }); ; 
+
+const envFile = `export const environment = { 
+    EMAILJS_SERVICE_ID: ' ${process.env. EMAILJS_SERVICE_ID} ', 
+    EMAILJS_TEMPLATE_ID: ' ${process.env. EMAILJS_TEMPLATE_ID} ', 
+    EMAILJS_USER_ID: ' ${process.env. EMAILJS_USER_ID} ', 
+}; 
+` ; 
+const targetPath = path. join (__dirname, './src/environments/environment.development.ts' ); 
+fs. writeFile (targetPath, envFile, ( err ) => { 
+    if (err) { 
+        console . error (err); 
+        throw err; 
+    } else { 
+        console . log (successColor, ` ${checkSign} Environment.development.ts gerado com sucesso` ); 
+    } 
+});
