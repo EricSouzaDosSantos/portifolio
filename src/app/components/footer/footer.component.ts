eafa46e1b44432e2a9dd72c7b2f26980d48476cd
Eric <ericsouza94895@gmail.com>
2025-02-12 23:06:45 -0300

    feat: add variable to save gmail and adding effect to change section

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -7,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  gmail: string = 'ericsouzadossantos28@gmail.com'
+  menuOpen: boolean = false;
+  borderRadius: string = "0 0 10px 10px";
+  isScrolling: boolean = false;
+
+   scrollToSection(sectionId: string): void {
+      const section = document.getElementById(sectionId);
+      if (section) {
+        const yOffset = -80;
+        const y = section.getBoundingClientRect().top + window.scrollY + yOffset;
+        window.scrollTo({
+          top: y,
+          behavior: 'smooth'
+        });
+      }
+      setTimeout(() => {
+        this.menuOpen = false;
+      }, 500);
+    }
 
 }
