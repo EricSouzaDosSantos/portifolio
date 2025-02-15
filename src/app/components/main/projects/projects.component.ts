08d5b91dfe1baeeb1e4823873c3300bd6321fed3
Eric <ericsouza94895@gmail.com>
2025-02-15 13:33:31 -0300

    feat: add message specifying that the project is still under development

--- src/app/components/main/projects/projects.component.ts
+++ src/app/components/main/projects/projects.component.ts
@@ -16,7 +16,7 @@ export class ProjectsComponent {
 
   projects = [
     {
-      title: 'Projeto Teste',
+      title: 'Ainda em desenvolviemnto',
       image: '/my-animation.png',
       videoId: 'VCJ45dH0aOs',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
@@ -25,7 +25,7 @@ export class ProjectsComponent {
       whatIDid: ['Desenvolvimento da API back-end com Java Spring Boot.', 'Deploy da API na AWS EC2 com configuração de VPC e Security Groups.', 'Configuração de Load Balancer para distribuir o tráfego.', 'Certificação SSL com ACM e domínio gerenciado pelo Route53.', 'Integração da API com o front-end para consumo de dados.', 'Liderança técnica da equipe, garantindo organização e entregas eficazes.'],
     },
     {
-      title: 'projeto',
+      title: 'Ainda em desenvolviemnto',
       image: '/my-animation.png',
       videoId: 'VCJ45dH0aOs',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
